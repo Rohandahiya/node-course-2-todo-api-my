b97462df398a5b46e83b0b6039e677bcccdf028a
Rohandahiya <dahiyarohan007@gmail.com>
2018-06-11 21:55:48 +0530

    random update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MANGODB_URI || 'mongodb://Rohan007:dahiyarohan784@ds255740.mlab.com:55740/todoapp');
+mongoose.connect(mongodb:Rohan007:dahiyarohan007@ds255740.mlab.com:55740/todoapp || 'mongodb://localhost:27017/TodoApp');
 
 module.exports={mongoose};
